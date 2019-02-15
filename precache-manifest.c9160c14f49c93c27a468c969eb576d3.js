f931eb4e35b28413db6c6e972d0bd9310a93cc52
vctrklndr <victor@nasa.se>
2019-02-15 12:14:48 +0100

    Updates

---
+++ precache-manifest.c9160c14f49c93c27a468c969eb576d3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5888a8f166e7cd25dd120e3c890a1166",
+    "url": "/MAVK-Spotify/static/media/noise.5888a8f1.png"
+  },
+  {
+    "revision": "f801a36277fef9657a41b4c5954506e1",
+    "url": "/MAVK-Spotify/static/media/glitch.f801a362.gif"
+  },
+  {
+    "revision": "0468c51db1dcc19ed74cd6f94810448f",
+    "url": "/MAVK-Spotify/static/media/computer.0468c51d.png"
+  },
+  {
+    "revision": "b57fdd065fc080701e1f3f364abfc2c3",
+    "url": "/MAVK-Spotify/static/media/GT-Pressura-Mono-Regular.b57fdd06.ttf"
+  },
+  {
+    "revision": "c4d3d5776693097097e8055cd41cafcb",
+    "url": "/MAVK-Spotify/static/media/Eurostile-Bold.c4d3d577.otf"
+  },
+  {
+    "revision": "8cf850a0e30025730fa3",
+    "url": "/MAVK-Spotify/static/js/runtime~main.8cf850a0.js"
+  },
+  {
+    "revision": "73c8acabb5dc6cb9cfde",
+    "url": "/MAVK-Spotify/static/js/main.73c8acab.chunk.js"
+  },
+  {
+    "revision": "0b4e6ed782cbf95827f0",
+    "url": "/MAVK-Spotify/static/js/2.0b4e6ed7.chunk.js"
+  },
+  {
+    "revision": "73c8acabb5dc6cb9cfde",
+    "url": "/MAVK-Spotify/static/css/main.0e3c2a9e.chunk.css"
+  },
+  {
+    "revision": "faea1c8389cc72621c4b121430032858",
+    "url": "/MAVK-Spotify/index.html"
+  }
+];
